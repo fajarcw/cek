2539940bbced40a7ef7af6cf904d532b6a061ef6
fajarcw <fajarcw@gmail.com>
2017-03-22 08:13:04 +0700

    Update app.js

--- app.js
+++ app.js
@@ -4,6 +4,6 @@ module['exports'] = function cobaBot (hook) {
     .post('https://api.telegram.org/bot' + hook.env.coba_bot_key + '/sendMessage')
     .form({
       "chat_id": hook.params.message.chat.id,
-      "text": "anda mengirim: " + hook.params.message.text
+      "text": "You said: " + hook.params.message.text
     });
 };
